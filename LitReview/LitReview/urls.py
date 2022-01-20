2ee23f274eac752c2f4ab2780f87e12f52897f2b
graquil <g.raquil@biomarel.com>
2022-01-20 23:49:28 +0100

    creation des templates et de la page de connexion

--- LitReview/LitReview/urls.py
+++ LitReview/LitReview/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import core.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', core.views.login_page, name='login'),
+    path('logout/', core.views.logout_user, name='logout'),
 ]
